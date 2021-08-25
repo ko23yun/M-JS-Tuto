9424f30cf5ec12530c93cce5789608b3e856d4d8
ko23yun <ko23yun@naver.com>
2021-08-25 16:20:06 +0900

    객체 공부 중!

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log(1);
+let user = {
+  name: "John",
+  age: 30,
+};
+
+let clone = {}; // 새로운 빈 객체
+
+// 빈 객체에 user 프로퍼티 전부를 복사해 넣습니다.
+for (let key in user) {
+  clone[key] = user[key];
+  console.log(user[key]);
+}
+
+// console.log(clone);
