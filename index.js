3b8afdb8533794093433565cc70cc2c7edf21627
ko23yun <ko23yun@naver.com>
2021-08-29 21:46:54 +0900

    5.5 배열과 메서드까지 완료

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-let arr = [1, 2, 3, 4, 5];
-
-for (let num in arr) {
-  console.log(num);
+function groupById(arr) {
+  return arr.reduce((obj, value) => {
+    obj[value.id] = value;
+    return obj;
+  }, {});
 }
+
+let users = [
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
+];
+
+let usersById = groupById(users);
+console.log(usersById);
+
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
