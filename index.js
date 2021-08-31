9b46beea38eb2a49934a58c0f9c7e3f64e23bee9
ko23yun <ko23yun@naver.com>
2021-08-31 16:10:50 +0900

    Date 객체와 날짜까지 공부만 완료

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-let time = new Date("2017-01-26");
+// 2012년 2월 20일, 오전 3시 12분을 나타내는 Date 객체를 만들어보세요(시간대는 로컬).
 
-console.log(time);
+// 그리고 alert 함수를 이용해 생성한 객체를 출력하세요.
+
+let answer = new Date(2012, 1, 20, 3, 12);
+
+console.log(answer);
