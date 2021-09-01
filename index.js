6c6291fd86cb4765e92b7b8a0da3d6d8f47a53ce
ko23yun <ko23yun@naver.com>
2021-09-01 21:32:51 +0900

    5.12 JSON과 메서드 공부 완료

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-function getSecondsToTomorrow() {
-  let now = new Date();
+let user = {
+  name: "John Smith",
+  age: 35,
+};
 
-  // 현재 년, 월, 일을 나타내는 객체를 생성
-  let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+let secondUser = JSON.parse(JSON.stringify(user));
 
-  let diff = tomorrow - now; // 차이(ms)
-  return Math.round(diff / 1000); // 초로 변환
-}
-
-console.log(getSecondsToTomorrow());
+console.log(secondUser);
