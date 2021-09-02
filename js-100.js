6bbc5dd37d1379a86f0fc152a6550a309811bf84
ko23yun <ko23yun@naver.com>
2021-09-02 22:46:19 +0900

    js-100 문제 풀이

--- js-100.js
+++ js-100.js
@@ -1,10 +1,4 @@
-// 데이터
-// <여기에 class를 작성하세요.>
+const fs = require("fs");
+const input = fs.readFileSync("./input.txt").toString();
 
-const x = new Wizard(545, 210, 10);
-console.log(x.health, x.mana, x.armor);
-x.attack();
-
-// 출력
-// 545 210 10
-// 파이어볼
+console.log(input === input.toUpperCase() ? "YES" : "NO");
